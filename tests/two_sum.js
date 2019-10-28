cd817df30f81969787df8d617eeedec442933c2d
showmeyourhits <smile4tme@gmail.com>
2019-10-28 20:38:39 +0300

    complete two sum

--- tests/two_sum.js
+++ tests/two_sum.js
@@ -11,11 +11,21 @@ describe('two_sum', () => {
             nums: [0,4,3,0],
             target: 0,
             result: [0,3]
-        }
+        },
+        {
+            nums: [-1,-2,-3,-4,-5],
+            target: -8,
+            result: [2, 4],
+        },
+        {
+            nums: [0,-2,-3,5],
+            target: 2,
+            result: [2, 3],
+        },
     ]
 
     cases.forEach(({nums, target, result}) => {
-        test(`nums: [${nums}], target: ${9}`, () => {
+        test(`nums: [${nums}], target: ${target}`, () => {
             expect(twoSum(nums, target)).toEqual(result)
         })
     })
--- two_sum.js
+++ two_sum.js
@@ -1,23 +1,24 @@
 var twoSum = function(nums, target) {
-    const minusTarget = nums.map(n => n - target);
-    let first = 0;
+    let hash = new Map();
+    let i = 0;
 
-    while (first < nums.length) {
-        if (minusTarget[first] < 0) {
-            const abs = Math.abs(minusTarget[first]);
-            let second = 0;
-            
-            while (second < nums.length) {
-                if (second !== first && nums[second] === abs) {
-                    return [first, second];
-                }
-                second++;
-            }
-        }
-        first++;
+    while (i < nums.length) {
+        hash.set(nums[i], i);
+        i++;
     }
 
-    return [-1,-1];
+    i = 0;
+
+    while (i < nums.length) {
+        const comp = target - nums[i];
+        if (hash.has(comp) && hash.get(comp) !== i) {
+            return [i, hash.get(comp)]
+        }
+        i++;
+    }
+
+
+    return [-1, -1];
 };
 
 module.exports = twoSum;
